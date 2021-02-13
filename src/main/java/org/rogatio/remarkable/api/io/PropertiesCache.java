10c57097e470f9a4218cb0fd5a8a9f17a03c4a02
Matthias Wegner <mic.mat.wegner@web.de>
2021-02-13 23:46:34 +0100

    Reduce POM

--- src/main/java/org/rogatio/remarkable/api/io/PropertiesCache.java
+++ src/main/java/org/rogatio/remarkable/api/io/PropertiesCache.java
@@ -29,7 +29,6 @@ import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.rogatio.remarkable.api.Starter;
 
 /**
  * The Class PropertiesCache.
